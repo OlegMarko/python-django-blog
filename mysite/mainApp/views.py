b0efdd0056f615cbcdfb74eec733cf9d4cd48417
Oleg Marko <olegmarko7@gmail.com>
2018-09-04 08:54:37 +0300

    Added html templates

--- mysite/mainApp/views.py
+++ mysite/mainApp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'mainApp/homePage.html')
+
+
+def contact(request):
+    return render(request, 'mainApp/basic.html', {'values': [
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A minima optio provident velit? Doloremque, praesentium!',
+        '(098) 123 45 67'
+    ]})
